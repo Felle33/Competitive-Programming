c4927150d58c62c2be7dc17c9a8ee699a7a10289
tommi <tommi.felle@gmail.com>
2023-12-21 16:52:22 +0100

    fixed the problem fixed_legth_paths now goes in TLE and another equal one on codeforces is AC

--- usaco/platinum/centroid_decomposition/fixed_length_paths.cpp
+++ usaco/platinum/centroid_decomposition/fixed_length_paths.cpp
@@ -40,12 +40,13 @@ int n, k;
 vi adj[MAX_N];
 int subtreeSize[MAX_N];
 bool removed[MAX_N];
-bool visited[MAX_N];
-vector<int> to_remove_visited;
+//bool visited[MAX_N];
+//vector<int> to_remove_visited;
+int mx_depth;
 
 int parent[MAX_N];
 ll centroid_array[MAX_N];
-ll subtree_array[MAX_N];
+//ll subtree_array[MAX_N];
 
 queue<int> q;
 ll ans;
@@ -61,32 +62,18 @@ void calc_subtree_size(int u, int p) {
     }
 }
 
-void bfs(int start) {
-    int distance = 1;
-    q.push(start);
-    visited[start] = 1;
+void get_cnt(int node, int parent, bool filling, int depth = 1) {
+	if (depth > k) return;
 
-    while(!q.empty()) {
-        int size = q.size();
+	mx_depth = max(mx_depth, depth);
 
-        while(size--) {
-            int u = q.front();
-            q.pop();
-            subtree_array[distance]++;
-
-            for(int to : adj[u]) {
-                if(!removed[to] && !visited[to]) {
-                    q.push(to);
-                    visited[to] = 1;
-                    to_remove_visited.pb(to);
-                }
-            }
-        }
-        distance++;
-    }
+	if (filling) centroid_array[depth]++;
+	else ans += centroid_array[k - depth];
+	for (int i : adj[node])
+		if (!removed[i] && i != parent) get_cnt(i, node, filling, depth + 1);
 }
 
-void find_centroid(int entryPoint, int level) {
+void find_centroid(int entryPoint) {
     calc_subtree_size(entryPoint, entryPoint);
 
     int centroid = entryPoint;
@@ -122,35 +109,22 @@ void find_centroid(int entryPoint, int level) {
     printf("\n\n");*/
 
     removed[centroid] = 1;
-
     centroid_array[0] = 1;
+    mx_depth = 0;
     for(int to : adj[centroid]) {
         if(!removed[to]) {
-            bfs(to);
-
-            for(int i = 1; i <= k && subtree_array[i] > 0 ; i++) {
-                ans += subtree_array[i] * centroid_array[k - i];
-            }
-
-            for(int i = 1; subtree_array[i] > 0; i++) {
-                centroid_array[i] += subtree_array[i];
-                subtree_array[i] = 0;
-            }
+            get_cnt(to, centroid, false);
+            get_cnt(to, centroid, true);
         }
     }
     
-    for(int i = 1; centroid_array[i] > 0; i++) {
+    for(int i = 1; i <= mx_depth; i++) {
         centroid_array[i] = 0;
     }
 
-    for(int u : to_remove_visited) {
-        visited[u] = 0;
-    }
-    to_remove_visited.clear();
-
     for(int to : adj[centroid]) {
         if(!removed[to]) {
-            find_centroid(to, level + 1);
+            find_centroid(to);
         }
     }
 }
@@ -164,7 +138,7 @@ void solve(){
         adj[b].pb(a);
     }
 
-    find_centroid(0, 0);
+    find_centroid(0);
 
     cout << ans << '\n';
 }
@@ -189,4 +163,6 @@ int main(){
 8 6
 9 2
 10 6
+
+ans = 10
 */
